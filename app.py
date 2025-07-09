2463e69c2de740a43e03cd8e148767417e833efb
Yonathan Geleta <yonathan.geleta@seo-usa.org>
2025-07-09 16:07:28 +0000

    added update-server endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for, flash, redirect
+import git
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm
 from flask_behind_proxy import FlaskBehindProxy
 
@@ -19,5 +20,14 @@ def register():
         return redirect(url_for('home')) # if so - send to home page
     return render_template('register.html', title='Register', form=form)
 
+@app.route("/update_server", methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('/home/hostdemo/host-demo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
